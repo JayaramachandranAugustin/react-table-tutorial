0659f7a5a2ff9b11675dc268669b8828bb5d9c26
Jayaramachandran <david.david188@gmail.com>
2021-08-31 23:25:05 -0500

    added async debounce

--- src/GlobalFilter.js
+++ src/GlobalFilter.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAsyncDebounce } from 'react-table'
 
 const GlobalFilter = ({filter,setFilter}) => {
-    const changeHandle =(event)=>{
-        setFilter(event.target.value)
+
+    const [filterText, setFilterText] = useState(filter)
+ 
+    const debounce = useAsyncDebounce(value=>{
+        setFilter(value)
+    },1000)
+
+    const handleChange =(event)=>{
+        setFilterText(event.target.value)
+        debounce(event.target.value)
     }
 
     return (
         <span>
-            <input className="global-filter-input" value={filter || ''} placeholder="Search " onChange={changeHandle}/>
+            <input className="global-filter-input" value={filterText || ''} placeholder="Search " onChange={handleChange}/>
         </span>
     )
 }
